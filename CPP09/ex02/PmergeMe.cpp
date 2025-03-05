09fa988cc613cb6dcb9284c81d0b72466045fd8c
Timo Saari <timo@timosaari.com>
2025-03-05 14:47:12 +0200

    added jacobstahl insert sorting, segfault with big numbers

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,21 +6,22 @@
 /*   By: tsaari <tsaari@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 08:24:08 by tsaari            #+#    #+#             */
-/*   Updated: 2025/02/24 12:56:07 by tsaari           ###   ########.fr       */
+/*   Updated: 2025/03/05 14:44:26 by tsaari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 #include <iostream>
 #include <algorithm>
+#include <climits>
 
 
 PmergeMe::PmergeMe() {}
 
-PmergeMe::PmergeMe(char **argv, ContainerType type) : listOrphan(0), vectorOrphan(0), isOrphan(false)
+PmergeMe::PmergeMe(char **argv, ContainerType type) : dequeOrphan(-1), vectorOrphan(-1), isOrphan(false)
 {
-	if (type == LIST)
-		loadListFromArgs(argv);
+	if (type == DEQUE)
+		loadDequeFromArgs(argv);
 	else if (type == VECTOR)
 		loadVectorFromArgs(argv);
 }
@@ -36,70 +37,71 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 	return *this;
 }
 
+PmergeMe::~PmergeMe() {}
 
-std::list <unsigned int> PmergeMe::getOriginalList() const {
-	return _originalList;
+
+//----------------------------GETTERS-------------------------------------
+
+std::deque <int> PmergeMe::getOriginalDeque() const {
+	return _originalDeque;
 }
 
-std::vector <unsigned int> PmergeMe::getOriginalVector() const {
+
+std::vector <int> PmergeMe::getOriginalVector() const {
 	return _originalVector;
 }
 
-std::list <std::pair<unsigned int, unsigned int>> PmergeMe::getListPairs() const {
-	return _listPairs;
+std::deque <std::pair<int, int>> PmergeMe::getDequePairs() const {
+	return _dequePairs;
 }
 
-std::vector <std::pair<unsigned int, unsigned int>> PmergeMe::getVectorPairs() const {
+std::vector <std::pair<int, int>> PmergeMe::getVectorPairs() const {
 	return _vectorPairs;
 }
 
-std::list <unsigned int> PmergeMe::getFinalList() const {
-	return _finalList;
+
+std::deque <int> PmergeMe::getFinalDeque() const {
+	return _finalDeque;
 }
 
-std::vector <unsigned int> PmergeMe::getFinalVector() const {
+std::vector <int> PmergeMe::getFinalVector() const {
 	return _finalVector;
 }
 
+//----------------------------LOAD FUNCTIONS-------------------------------------
 
-PmergeMe::~PmergeMe() {}
-
-void PmergeMe::loadListFromArgs(char **argv)
-{	
-	//making list from args
-	for (unsigned int i = 1; argv[i] != nullptr; i++)
+void PmergeMe::loadDequeFromArgs(char **argv)
+{
+	//deque from args
+	for (int i = 1; argv[i] != nullptr; i++)
 	{
-		unsigned int num = std::stoi(argv[i]); 
+		int num = std::stoi(argv[i]);
 		if (num < 0)
 			throw NegativeNumberException();
-
-		_originalList.push_back(num);
+		_originalDeque.push_back(num);
 	}
-	//taking care of orphan
-	if (_originalList.size() % 2 != 0)
+	//take care of orphan
+	if (_originalDeque.size() % 2 != 0)
 	{
-		listOrphan = _originalList.back();
-		_originalList.pop_back(); 
+		dequeOrphan = _originalDeque.back();
+		_originalDeque.pop_back();
 		isOrphan = true;
 	}
-	//making pairs to list
-	auto it = _originalList.begin();
-	while (it != _originalList.end()) {
-		unsigned int  first = *it;
-		if (++it != _originalList.end()) {
-			_listPairs.emplace_back(first, *it);
-			++it;
-		}
+	//make pairs
+	for (size_t i = 0; i < _originalDeque.size(); i += 2) {
+		_dequePairs.emplace_back(_originalDeque[i], _originalDeque[i + 1]);
 	}
+	if (isOrphan)
+		_originalDeque.push_back(dequeOrphan);
 }
 
 
 void PmergeMe::loadVectorFromArgs(char **argv)
 {
 	//making vector from args
-	for (unsigned int i = 1; argv[i] != nullptr; i++)
+	for (int i = 1; argv[i] != nullptr; i++)
 	{
-		unsigned int num = std::stoi(argv[i]);
+		int num = std::stoi(argv[i]);
 		if (num < 0)
 			throw NegativeNumberException();
 		_originalVector.push_back(num);
@@ -115,89 +117,160 @@ void PmergeMe::loadVectorFromArgs(char **argv)
 	for (size_t i = 0; i < _originalVector.size(); i += 2) {
 		_vectorPairs.emplace_back(_originalVector[i], _originalVector[i + 1]);
 	}
+	if(isOrphan)
+		_originalVector.push_back(vectorOrphan);
 }
 
+//----------------------------DEQUE-------------------------------------
 
-void PmergeMe::mergeSortList(std::list<std::pair<unsigned int, unsigned int>>::iterator start,
-							 std::list<std::pair<unsigned int, unsigned int>>::iterator mid,
-							 std::list<std::pair<unsigned int, unsigned int>>::iterator end,
-							 std::list<std::pair<unsigned int, unsigned int>>& tempList)
-{
-	auto left = start;
-	auto right = mid;
-
-	tempList.clear();
-	
-	while (left != mid && right != end)
-	{
-		if (left->first < right->first)
-			tempList.push_back(*left++);
-		else
-			tempList.push_back(*right++);
+std::deque<int> generateJacobsthalDeque(int n) {
+	std::deque<int> jacobsthal = {0, 1};
+	while ((int)jacobsthal.size() < n) {
+		int next = jacobsthal[jacobsthal.size() - 1] + 2 * jacobsthal[jacobsthal.size() - 2];
+		jacobsthal.push_back(next);
 	}
-
-	while (left != mid)
-		tempList.push_back(*left++);
-
-	while (right != end)
-		tempList.push_back(*right++);
-
-	auto it = start;
-	for (auto& pair : tempList)
-		*it++ = pair;
+	return jacobsthal;
 }
 
+// Function to manually sort with minimal swaps
+void manualSort(std::deque<int>& arr) {
+	int n = arr.size();
+	for (int i = 1; i < n; i++) {
+		int j = i;
+		while (j > 0 && arr[j] < arr[j - 1]) {
+			std::swap(arr[j], arr[j - 1]); // Swap adjacent elements if out of order
+			j--;
+		}
+	}
+}
+
+void PmergeMe::insertUsingJacobsthalDeque() {
+    std::deque<int> jacobsthal = generateJacobsthalDeque(_dequePairs.size() + 2);
+    for (size_t i = 0; i < _dequePairs.size(); i++) {
+        // Ensure pos doesn't exceed the size of _finalDeque
+        size_t pos = std::min(jacobsthal[i], (int)_finalDeque.size());
+        
+        std::cout << "jacobsthal[" << i << "]: " << jacobsthal[i] << ", pos: " << pos << std::endl;
+
+        if (pos >= 0 && pos <= _finalDeque.size()) {
+            // Insert the second value of the pair at the calculated position
+            _finalDeque.insert(_finalDeque.begin() + pos, _dequePairs[i].second);
+        } else {
+            std::cerr << "Invalid position: " << pos << std::endl;
+        }
+    }
+    // Optionally, manually sort the deque if necessary
+    manualSort(_finalDeque);
+}
+/*void PmergeMe::insertUsingJacobsthalDeque() {                                            ////////segfaults
+	std::deque<int> jacobsthal = generateJacobsthalDeque(_dequePairs.size() + 2);
+	for (size_t i = 0; i < _dequePairs.size(); i++) {
+		size_t pos = std::min(jacobsthal[i], (int)_finalDeque.size());
+		std::cout << "pos: " << pos << std::endl;
+		if (pos >= 0 && pos <= _finalDeque.size())
+			_finalDeque.insert(_finalDeque.begin() + pos, _dequePairs[i].second);
+		else {
+			std::cerr << "Invalid position: " << pos << std::endl;
+		}
+	}
+	manualSort(_finalDeque);
+}*/
 
 
-
-void PmergeMe::mergeSplitList(std::list<std::pair<unsigned int, unsigned int>>::iterator start,
-							  std::list<std::pair<unsigned int, unsigned int>>::iterator end)
+void PmergeMe::mergeSortDeque(int start, int mid, int end, std::deque <std::pair<int , int>>& tempDeque)
 {
-	if (start == end || std::next(start) == end)
+	int i = start;
+	int j = mid + 1;
+	tempDeque.clear(); 
+
+	while (i <= mid && j <= end)
+	{
+		if (_dequePairs[i].first < _dequePairs[j].first)
+			tempDeque.push_back(_dequePairs[i++]);
+		else
+			tempDeque.push_back(_dequePairs[j++]);
+	}
+	while (i <= mid)
+	{
+		tempDeque.push_back(_dequePairs[i]);
+		i++;
+	}
+	while (j <= end)
+	{
+		tempDeque.push_back(_dequePairs[j]);
+		j++;
+	}
+	for (int k = 0; k < (int)tempDeque.size(); k++)
+		_dequePairs[start + k] = tempDeque[k];
+}
+
+void PmergeMe::mergeSplitDeque(int start, int end)
+{
+	 if (start >= end)
 		return;
 
-	auto mid = start;
-	std::advance(mid, std::distance(start, end) / 2);
-	mergeSplitList(start, mid);
-	mergeSplitList(mid, end);
-	std::list<std::pair<unsigned int, unsigned int>> tempList;
-	mergeSortList(start, mid, end, tempList);
+	int mid = (start + end) / 2;
+	mergeSplitDeque(start, mid);
+	mergeSplitDeque(mid + 1, end);
+	std::deque<std::pair<int, int>> tempDeque;
+	mergeSortDeque(start, mid, end, tempDeque); 
 }
 
-
-void PmergeMe::makeSortingWithList() {
-	for (auto& pair : _listPairs) {
+void PmergeMe::makeSortingWithDeque() {
+	for (auto& pair : _dequePairs) {
 		if (pair.first < pair.second) {
 			std::swap(pair.first, pair.second);
 		}
 	}
-	mergeSplitList(_listPairs.begin(), _listPairs.end());
-	
-	for(auto& pair : _listPairs) {
-		_finalList.push_back(pair.first);
+	mergeSplitDeque(0, _dequePairs.size() - 1);
+	for(auto& pair : _dequePairs) {
+		_finalDeque.push_back(pair.first);
 	}
-	for (auto& pair : _listPairs) {
-		auto it = _finalList.begin();
-		while (it != _finalList.end() && *it < pair.second) {
-			++it;
-		}
-		_finalList.insert(it, pair.second);
+	insertUsingJacobsthalDeque();
+	if (isOrphan)
+		_finalDeque.insert(std::lower_bound(_finalDeque.begin(), _finalDeque.end(), dequeOrphan), dequeOrphan);
+}
+
+
+
+
+//----------------------------Vector-------------------------------------
+
+std::vector<int> generateJacobsthalVector(int n) {
+	std::vector<int> jacobsthal = {0, 1};  // Base cases
+	while ((int)jacobsthal.size() < n) {
+		int next = jacobsthal[jacobsthal.size() - 1] + 2 * jacobsthal[jacobsthal.size() - 2];
+		jacobsthal.push_back(next);
 	}
-	//setting orphan
-	if (isOrphan) {
-		auto it = _finalList.begin();
-		while (it != _finalList.end() && *it < listOrphan) {
-			++it;
+	return jacobsthal;
+}
+
+void manualSort(std::vector<int>& arr) {
+	int n = arr.size();
+	for (int i = 1; i < n; i++) {
+		int j = i;
+		while (j > 0 && arr[j] < arr[j - 1]) {
+			std::swap(arr[j], arr[j - 1]); // Swap adjacent elements if out of order
+			j--;
 		}
-		_finalList.insert(it, listOrphan);
 	}
 }
 
 
-void PmergeMe::mergeSortVector(unsigned int start, unsigned int mid, unsigned int end, std::vector <std::pair<unsigned int , unsigned int>>& tempVector)
+void PmergeMe::insertUsingJacobsthalVector() {
+	std::vector<int> jacobsthal = generateJacobsthalVector(_vectorPairs.size() + 2);
+	for (size_t i = 0; i < _vectorPairs.size(); i++) {
+		int pos = std::min(jacobsthal[i], (int)_finalVector.size()); 
+		_finalVector.insert(_finalVector.begin() + pos, _vectorPairs[i].second); 
+	}
+	manualSort(_finalVector);
+}
+
+
+void PmergeMe::mergeSortVector(int start, int mid, int end, std::vector <std::pair<int , int>>& tempVector)
 {
-	unsigned int i = start;
-	unsigned int j = mid + 1;
+	int i = start;
+	int j = mid + 1;
 	tempVector.clear(); 
 
 	while (i <= mid && j <= end)
@@ -217,21 +290,21 @@ void PmergeMe::mergeSortVector(unsigned int start, unsigned int mid, unsigned in
 		tempVector.push_back(_vectorPairs[j]);
 		j++;
 	}
-	for (unsigned int k = 0; k < tempVector.size(); k++)
+	for (int k = 0; k < (int)tempVector.size(); k++)
 		_vectorPairs[start + k] = tempVector[k];
 }
 
 
 
-void PmergeMe::mergeSplitVector(unsigned int start, unsigned int end)
+void PmergeMe::mergeSplitVector(int start, int end)
 {
 	 if (start >= end)
 		return;
 
-	unsigned int mid = (start + end) / 2;
+	int mid = (start + end) / 2;
 	mergeSplitVector(start, mid);
 	mergeSplitVector(mid + 1, end);
-	std::vector<std::pair<unsigned int, unsigned int>> tempVector;
+	std::vector<std::pair<int, int>> tempVector;
 	mergeSortVector(start, mid, end, tempVector); 
 }
 
@@ -245,14 +318,9 @@ void PmergeMe::makeSortingWithVector() {
 	for(auto& pair : _vectorPairs) {
 		_finalVector.push_back(pair.first);
 	}
-	for (auto& pair : _vectorPairs) {
-		auto it = std::lower_bound(_finalVector.begin(), _finalVector.end(), pair.second);
-		_finalVector.insert(it, pair.second);
-	}
-	if (isOrphan) {
-		auto it = std::lower_bound(_finalVector.begin(), _finalVector.end(), vectorOrphan);
-		_finalVector.insert(it, vectorOrphan);
-	}
+	insertUsingJacobsthalVector();
+	if (isOrphan)
+		_finalVector.insert(std::lower_bound(_finalVector.begin(), _finalVector.end(), vectorOrphan), vectorOrphan);
 }
 
 
